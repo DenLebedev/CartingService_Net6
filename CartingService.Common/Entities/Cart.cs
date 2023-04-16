44196e385863e7d520e038d4012f15d8af1f2ff3
Den V. Lebedev <dvl@inbox.ru>
2023-04-16 12:56:47 +0300

    Added BLL, DAL and Common layers.
    Added BLL, DAL and Common layers.
    Added vain entities: Cart and Item

---
+++ CartingService.Common/Entities/Cart.cs
@@ -0,0 +1,8 @@
+﻿namespace CartingService.Common.Entities
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public List<int> ItemsIDs { get; set; }
+    }
+}
---
+++ CartingService.Common/Entities/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace CartingService.Common.Entities
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Item() { }
+    }
+}
